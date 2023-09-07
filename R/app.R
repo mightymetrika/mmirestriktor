a1164afcdd58f49ee1b71eb6601000b77879f8f9
Mackson Ncube <macksonncube@gmail.com>
2023-09-07 15:58:48 +0000

    app formatting

--- R/app.R
+++ R/app.R
@@ -13,12 +13,24 @@ mmirestriktor <- function(){
         shiny::actionButton("run_analysis", "Run Analysis")
       ),
       shiny::mainPanel(
+        shiny::uiOutput("variables_header"),
         shiny::verbatimTextOutput("variables"),
+        shiny::uiOutput("model_terms_header"),
         shiny::verbatimTextOutput("model_terms"),
-        shiny::verbatimTextOutput("iht_results"),
-        shiny::verbatimTextOutput("iht_interpretation"),
-        shiny::verbatimTextOutput("rm_results"),
-        shiny::verbatimTextOutput("rm_interpretation")
+        shiny::conditionalPanel(
+          condition = "input.analysis_type.includes('Informative Hypothesis Test')",
+          shiny::uiOutput("iht_results_header"),
+          shiny::verbatimTextOutput("iht_results"),
+          shiny::uiOutput("iht_interpretation_header"),
+          shiny::htmlOutput("iht_interpretation")
+        ),
+        shiny::conditionalPanel(
+          condition = "input.analysis_type.includes('Restricted Means')",
+          shiny::uiOutput("rm_results_header"),
+          shiny::verbatimTextOutput("rm_results"),
+          shiny::uiOutput("rm_interpretation_header"),
+          shiny::htmlOutput("rm_interpretation")
+        )
       )
     )
   )
@@ -30,11 +42,13 @@ mmirestriktor <- function(){
     })
 
     model <- NULL
+    model_fitted <- shiny::reactiveVal(FALSE)  # Create a reactive value to track the model fitting status
 
     shiny::observeEvent(input$fit_model, {
       shiny::req(input$formula, data(), input$engine)
       args_list <- list(formula = stats::as.formula(input$formula), data = data(), engine = input$engine, standardize = TRUE)
       model <<- do.call(mmir_model, args_list)
+      model_fitted(TRUE)  # Set the reactive value to TRUE after fitting the model
       output$model_terms <- shiny::renderPrint({
         names(stats::coef(model))
       })
@@ -42,6 +56,18 @@ mmirestriktor <- function(){
 
     output$variables <- shiny::renderPrint({
       names(data())
+    })
+
+    output$variables_header <- shiny::renderUI({
+      if(!is.null(data())){
+        shiny::tags$h4("Data Frame Variables")
+      }
+    })
+
+    output$model_terms_header <- shiny::renderUI({
+      if(model_fitted()){  # Use the reactive value to control the rendering of the header
+        shiny::tags$h4("Available Terms for Constraint")
+      }
     })
 
     shiny::observeEvent(input$run_analysis, {
@@ -52,23 +78,29 @@ mmirestriktor <- function(){
       if ("Informative Hypothesis Test" %in% input$analysis_type) {
         iht_res <- restriktor::iht(model, constraints = constraint)
         output$iht_results <- shiny::renderPrint({iht_res})
-        output$iht_interpretation <- shiny::renderPrint({
-          iht_interpreter(iht_res)
+        output$iht_interpretation <- shiny::renderUI({
+          shiny::HTML(nl2br(iht_interpreter(iht_res)))
         })
-      } else {
-        output$iht_results <- shiny::renderPrint({NULL})
-        output$iht_interpretation <- shiny::renderPrint({NULL})
+        output$iht_results_header <- shiny::renderUI({
+          shiny::tags$h4("Informative Hypothesis Test Results")
+        })
+        output$iht_interpretation_header <- shiny::renderUI({
+          shiny::tags$h4("Informative Hypothesis Test Interpretation")
+        })
       }
 
       if ("Restricted Means" %in% input$analysis_type) {
         rm_res <- restriktor::restriktor(model, constraints = constraint)
         output$rm_results <- shiny::renderPrint({rm_res})
-        output$rm_interpretation <- shiny::renderPrint({
-          rm_interpreter(rm_res)
+        output$rm_interpretation <- shiny::renderUI({
+          shiny::HTML(nl2br(rm_interpreter(rm_res)))
         })
-      } else {
-        output$rm_results <- shiny::renderPrint({NULL})
-        output$rm_interpretation <- shiny::renderPrint({NULL})
+        output$rm_results_header <- shiny::renderUI({
+          shiny::tags$h4("Restricted Means Results")
+        })
+        output$rm_interpretation_header <- shiny::renderUI({
+          shiny::tags$h4("Restricted Means Interpretation")
+        })
       }
     })
   }
