4dfb7c9eca7a2529d0dd43fec19189e249db1505
Mackson Ncube <macksonncube@gmail.com>
2023-09-11 16:00:20 +0000

    updated data structures and code for game (game still not working)

--- R/fbc_game_mgmt.R
+++ R/fbc_game_mgmt.R
@@ -1,3 +1,18 @@
+# deal_cards_to_grid <- function(deck = mmcards::shuffle_deck(), n) {
+#   # Deal n^2 cards from the deck
+#   dealt_cards <- vector("list", n^2)
+#
+#   for(i in 1:(n^2)) {
+#     deck <- mmcards::deal_card(deck)
+#     dealt_cards[[i]] <- deck$dealt_card
+#   }
+#
+#   # Convert the list of dealt cards into a matrix
+#   cards_matrix <- matrix(dealt_cards, nrow = n, ncol = n, byrow = TRUE)
+#
+#   return(list(cards_matrix = cards_matrix, updated_deck = deck$updated_deck))
+# }
+
 deal_cards_to_grid <- function(deck = mmcards::shuffle_deck(), n) {
   # Deal n^2 cards from the deck
   dealt_cards <- vector("list", n^2)
@@ -7,8 +22,8 @@ deal_cards_to_grid <- function(deck = mmcards::shuffle_deck(), n) {
     dealt_cards[[i]] <- deck$dealt_card
   }
 
-  # Convert the list of dealt cards into a matrix
-  cards_matrix <- matrix(dealt_cards, nrow = n, ncol = n, byrow = TRUE)
+  # Convert the list of dealt cards into a list of lists (each representing a row)
+  cards_matrix <- lapply(split(dealt_cards, rep(1:n, each=n)), function(row) as.list(row))
 
   return(list(cards_matrix = cards_matrix, updated_deck = deck$updated_deck))
 }
